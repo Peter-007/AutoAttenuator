9ab6a3a64da410d184a751ca7cddde788f85a55c
Peter-007 <Petet_007@aliyun.com>
2017-03-03 12:43:56 +0800

    add some comments

--- Attenuator.py
+++ Attenuator.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
-import time
+
 '''
 管脚定义：
             Raspberry
-衰减器1   GPIO_Name        PIN
+衰减器1   GPIO_Name   PIN
   1蓝 -----	GPIO.0	 11
   2橙 -----	GPIO.1	 12
   3绿 -----	GPIO.2	 13
@@ -12,11 +12,11 @@ import time
   6白 -----	GPIO.5	 18
   7红 -----	GPIO.29	 40
   8黑 -----		
-  9黄 -----	5V	2
- 10紫 -----  0V	6
+  9黄 -----	5V	    2
+ 10紫 -----  0V  	6
   
-衰减器2   GPIO_Name      PIN
- 1蓝 ----- GPIO.22       31
+衰减器2   GPIO_Name  PIN
+ 1蓝 ----- GPIO.22  31
  2橙 ----- GPIO.23	33
  3绿 ----- GPIO.24	35
  4棕 ----- GPIO.25	37
@@ -24,63 +24,34 @@ import time
  6白 ----- GPIO.27	36
  7红 ----- GPIO.28	38
  8黑 -----
- 9黄 ----- 5V	4
-10紫 ----- 0V	39  
+ 9黄 ----- 5V    	4
+10紫 ----- 0V    	39
 '''
+
 class ATT(object):
-    
+
+    # AttNumber: 可编程衰减器模块序号，本系统是有两个模块，分别为1和2
     def __init__(self,AttNumber):
-        Pins=((40,18,16,15,13,12,11),(38,36,32,37,35,33,31))
-        self.listPin = Pins[AttNumber-1]
+        Pins=((40,18,16,15,13,12,11),(38,36,32,37,35,33,31))    #定义管脚列表，为了便于同二进制的高地位顺序匹配，按管脚序号有大到小进行定义
+        self.listPin = Pins[AttNumber-1]    #获取指定可编程衰减器模块序号的管脚列表，用于SetAtt函数
+
+        # 初始化树莓派gpio接口
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.listPin,GPIO.OUT)
-        GPIO.output(self.listPin,0)
-        GPIO.setup(22,GPIO.IN)
-        GPIO.setup(29,GPIO.OUT)
-        GPIO.output(29,1)
+        GPIO.output(self.listPin,0)     #将可编程衰减器的所有管脚电平设为低，得到最大衰减值63.5dB
     
     def __del__(self):
         GPIO.cleanup()
 
+    # Val: 期望设置的衰减值，需要是0.5的倍数
     def SetAtt(self,Val):
-        n=int(round(Val*2))
-        strBin='{:07b}'.format(n)
-        listVal = [not bool(int(x)) for x in strBin]
-        #print Val,n,listVal
-        GPIO.output(self.listPin,listVal)
-        
-    def AutoAtt(self,start=0,stop=30,step=3,delay=1,repeattimes=1):
-        bStop = False
-        for j in range(repeattimes):
-            print('Repeat times: %d/%d' % (j+1,repeattimes))
-            for i in range(int(start),int(stop+step),int(step)):
-                bStop = self.IsStart()
-                if bStop:
-                    return
-                self.SetAtt(i)
-                time.sleep(delay)   #Unit:second
-                
-    def Blink(self,pin):
-        GPIO.setup(pin,GPIO.OUT)
-        for i in range(10):
-            GPIO.output(pin,not GPIO.input(pin))
-            time.sleep(0.5)
-            
-    def IsStart(self):
-        return GPIO.input(22)
-        
-
+        n=int(round(Val*2))     #可编程衰减器的步进是0.5dB,也即实现1dB的衰减，对应的管脚二进制值应该是2
+        strBin='{:07b}'.format(n)   #转换程7个bit位的二进制字符串
+        listVal = [not bool(int(x)) for x in strBin]  #可编程衰减器是低电平有效，故而对每个bit位取反
+        GPIO.output(self.listPin,listVal)   #将转换后的二进制列表值同时配置给可编程衰减器所有7个管脚
 
 if __name__ == '__main__':
-    
-    print('AutoAtt Start ' + time.strftime('%H:%M:%S',time.localtime()))
-
     att1 = ATT(1)
-    print(att1.IsStart())
-    att1.AutoAtt(63,0,-1,0.05,1000)
-    del att1    
-
-    print('AutoAtt End ' + time.strftime('%H:%M:%S',time.localtime()))
-
+    att1.SetAtt(10)
 
--- AutoAttGui.py
+++ AutoAttGui.py
@@ -7,7 +7,7 @@ import time, threading
 from ui_AutoAtt import Ui_FormAtt
 
 
-# from Attenuator import ATT
+from Attenuator import ATT
 
 class FormAtt(QWidget):
     def __init__(self, parent=None):
@@ -16,9 +16,13 @@ class FormAtt(QWidget):
         self.ui = Ui_FormAtt()
         self.ui.setupUi(self)
 
-        # self.att1 = ATT(1)
-        # self.att2 = ATT(2)
+        self.att1 = ATT(1)
+        self.att2 = ATT(2)
 
+    # 创建循环配置的衰减值列表,如：[0,3,6,9,6,3]
+    # Start: 开始衰减值
+    # Finish:终止衰减值
+    # Step: 步进值
     def CreateAttList(self, Start, Finish, Step):
         lst1 = list(range(Start, Finish + int(Step / abs(Step)), Step))
         lstResult = []
@@ -30,8 +34,10 @@ class FormAtt(QWidget):
         print(lstResult)
         return lstResult
 
+    # 根据用户在界面配置的开始／终止／步进／时间间隔／重复次数的参数值自动周期性的改变1号衰减器模块的值
     def autoSetAtt1(self):
         if self.ui.checkBox1.isChecked():
+            # 改变界面控件状态，使得用户不能在自动控制衰减器模块期间，进行其他操作
             self.ui.pushButton_AutoSet.setEnabled(False)
             self.ui.pushButton_Close.setEnabled(False)
             self.ui.spinBox_Current1.setEnabled(False)
@@ -50,12 +56,14 @@ class FormAtt(QWidget):
             self.ui.spinBox_Current1.setValue(iStart)
             self.ui.spinBox_Current1.setEnabled(True)
 
-            if threading.active_count() == self.iThreadCount + 1:
+            if threading.active_count() == self.iThreadCount + 1:  #如果两个线程结束，打开相应按钮的使能
                 self.ui.pushButton_AutoSet.setEnabled(True)
                 self.ui.pushButton_Close.setEnabled(True)
 
+    # 根据用户在界面配置的开始／终止／步进／时间间隔／重复次数的参数值自动周期性的改变2号衰减器模块的值
     def autoSetAtt2(self):
         if self.ui.checkBox1.isChecked():
+            # 改变界面控件状态，使得用户不能在自动控制衰减器模块期间，进行其他操作
             self.ui.pushButton_AutoSet.setEnabled(False)
             self.ui.pushButton_Close.setEnabled(False)
             self.ui.spinBox_Current1.setEnabled(False)
@@ -74,20 +82,23 @@ class FormAtt(QWidget):
             self.ui.spinBox_Current2.setValue(iStart)
             self.ui.spinBox_Current2.setEnabled(True)
 
-            if threading.active_count() == self.iThreadCount + 1:
+            if threading.active_count() == self.iThreadCount + 1:     #如果两个线程结束，打开相应按钮的使能
                 self.ui.pushButton_AutoSet.setEnabled(True)
                 self.ui.pushButton_Close.setEnabled(True)
 
+    # 定义一个Slot，实时的改变1号衰减器模块的值
     @pyqtSlot(int)
     def on_spinBox_Current1_valueChanged(self, value):
         print('Set Attenuator-1 value: %d dB' % value)
         # self.att1.SetAtt(value)
 
+    # 定义一个Slot， 实时的改变2号衰减器模块的值
     @pyqtSlot(int)
     def on_spinBox_Current2_valueChanged(self, value):
         print('Set Attenuator-2 value: %d dB' % value)
         # self.att2.SetAtt(value)
 
+    #创建两个线程实现两个衰减器模块值周期性的自动改变衰减值
     @pyqtSlot()
     def on_pushButton_AutoSet_clicked(self):
         self.iThreadCount = threading.activeCount()
@@ -96,7 +107,7 @@ class FormAtt(QWidget):
         t2 = threading.Thread(target=self.autoSetAtt2)
         t1.start()
         t2.start()
-        # self.iThreadCount = threading.activeCount()
+        self.iThreadCount = threading.activeCount()
 
 
 if __name__ == '__main__':
